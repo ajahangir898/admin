f23675b0b9b80520c1678d09cabcfb989900adb6
unknown <ajahangir206@gmail.com>
2026-01-02 10:27:30 -0800

    Fix: marquee single text scroll, footer logo size

--- components/store/header/AdminNoticeTicker.tsx
+++ components/store/header/AdminNoticeTicker.tsx
@@ -9,12 +9,9 @@ export const AdminNoticeTicker: React.FC<AdminNoticeTickerProps> = ({ noticeText
 
   return (
     <div className="w-full bg-white border-b border-gray-100 py-1.5 overflow-hidden">
-      <div className="relative overflow-hidden">
-        <div className="flex animate-marquee-seamless" style={{ color: 'rgb(var(--color-font-rgb))' }}>
-          <span className="text-sm whitespace-nowrap px-8">{noticeText}</span>
-          <span className="text-sm whitespace-nowrap px-8">{noticeText}</span>
-          <span className="text-sm whitespace-nowrap px-8">{noticeText}</span>
-          <span className="text-sm whitespace-nowrap px-8">{noticeText}</span>
+      <div className="marquee-container">
+        <div className="marquee-content">
+          <span className="text-sm" style={{ color: 'rgb(var(--color-font-rgb))' }}>{noticeText}</span>
         </div>
       </div>
     </div>

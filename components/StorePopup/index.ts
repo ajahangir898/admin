0dcafcb25ed38d650a24e6a52af5e1922f9fe437
unknown <ajahangir206@gmail.com>
2026-01-03 09:34:44 -0800

    Refactor: move StorePopup into store folder

--- components/StorePopup/index.ts
+++ components/StorePopup/index.ts
@@ -1,2 +1,5 @@
-export { StorePopup } from './StorePopup';
-export type { StorePopupProps } from './StorePopup';
+// Backward-compatible re-export.
+// Existing code lazy-loads from '../components/StorePopup'.
+export { StorePopup } from '../store/StorePopup/StorePopup.tsx';
+export type { StorePopupProps } from '../store/StorePopup/StorePopup.tsx';
+
